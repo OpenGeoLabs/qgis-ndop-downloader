685ce9897a8738edf1f9e4b5e01950d85f816e6a
Jachym Cepicky <jachym.cepicky@gmail.com>
2019-03-03 10:53:57 +0100

    preparing to dist

--- setup.py
+++ setup.py
@@ -10,27 +10,29 @@ from setuptools import setup
 with open("requirements.txt") as f:
     install_requirements = f.read().splitlines()
 
-setup(name='nd_aopk',
+setup(name='ndop-downloader',
         author='Oto Kaláb',
         author_email='oto.kalab@opengeolabs.cz',
         maintainer="Oto Kaláb",
         maintainer_email='oto.kalab@opengeolabs.cz',
-        url='https://github.com/foo/bar/',
-        description='',
-        version="1.0.0",
-        packages=[],
+        url='https://github.com/OpenGeoLabs/qgis-ndop-downloader',
+        description='NDOP Downloader is an application for downloading of records of occurence from official database of the Nature Conservation Agency of the Czech Republic (Nálezová databáze Agentury ochrany přírody a krajiny ČR - NDOP - AOPK).',
+        version="0.1.0",
+        packages=["ndop"],
         long_description=open('README.md').read(),
-        scripts=["nd_aopk.py"],
-
+        scripts=["bin/ndop"],
+        license="GNU/GPL3",
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
+            'Development Status :: 4 - Beta',
             'Environment :: Console',
             'Intended Audience :: Science/Research',
-            'Natural Language :: English',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Natural Language :: Czech',
             'Operating System :: POSIX :: Linux',
             'Programming Language :: Python :: 3'
-            ],
+        ],
 
         install_requires=install_requirements,
-        keywords='GIS, AOPK, Nálezová databáze', 'Species occurences',
+        keywords=['GIS', 'AOPK', 'Nálezová databáze', 'Species occurences,'
+                  'NDOP', 'The Czech republic']
 )
